d54cc6f8c2208a85e7157f60eb9dccc645a3519c
Kamil Grondys <kamil.grondys@codecool.com>
2020-04-07 19:48:33 +0200

    adding first test
    Adding Matematyka_IsPrimaryNumber_Should test suite including
    IsPrimaryNumber_Input1_ReturnFalse method

--- Matematyka.Test/UnitTest1.cs
+++ Matematyka.Test/UnitTest1.cs
@@ -3,12 +3,13 @@ using Xunit;
 
 namespace Matematyka.Test
 {
-    public class UnitTest1
+    public class Matematyka_IsPrimeNumber_Should
     {
         [Fact]
-        public void Test1()
+        public void IsPrimeNumber_Input1_ReturnFalse()
         {
-
+            var result = Math.IsPrimeNumber(1);
+            Assert.False(result, "1 should not be a prime number!");
         }
     }
 }
---
+++ Matematyka/Math.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Matematyka
+{
+    public static class Math
+    {
+        public static bool IsPrimeNumber(int candidate)
+        {
+            if(candidate == 1){
+                return false;
+            }
+            throw new NotImplementedException("Not implemented");
+        }
+    }
+}
